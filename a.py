1661adec440b9d5b002b1c5e1332b982cffd753e
R4mbb <yong010301@gmail.com>
2024-12-11 17:42:10 +0900

    2024-12-11

--- a.py
+++ a.py
@@ -1,1 +1,10 @@
-print(1)
+import time
+start_time = time.time()
+
+for _ in range(10000):
+    for _ in range(10000):
+        pass
+
+end_time = time.time()
+
+print(f"time : {end_time - start_time}")
