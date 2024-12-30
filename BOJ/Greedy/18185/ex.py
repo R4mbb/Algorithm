88266f3546e39d07efabe719b3e88779bd2e57d7
R4mbb <yong010301@gmail.com>
2024-12-30 22:38:35 +0900

    2024-12-30

--- BOJ/Greedy/18185/ex.py
+++ BOJ/Greedy/18185/ex.py
@@ -1,36 +1,49 @@
 # 시간초과
-n = int(input())
-data = list(map(int, input().split()))
+import sys, time
+#n = int(input())
+n = int(sys.stdin.readline())
+data = list(map(int, sys.stdin.readline().split()))
 
 result = 0
-c = sum(data)
 
-while c > 0:
+a = time.time()
+while True:
     for num in range(n):
+        #print(f"data : {data}\nnum : {num}")
         if data[num] > 0:
-            if (num+1) <= len(data)-1:
-                if data[num+1] > 0 and data[num] <= data[num+1]:
-                    if (num+2) <= len(data)-1:
-                        if data[num+2] > 0 and data[num+1] <= data[num+2]:
-                            result += 7
-
-                            for j in range(num, num+3):
-                                data[j] -= 1
-                            break
-                
-                    result += 5
-
-                    for j in range(num, num+2):
-                        data[j] -= 1
+            if (num+2) <= len(data)-1:
+                if data[num+1] and data[num+2] > 0 and data[num+1] <= data[num+2] and data[num] <= data[num+1]:
+                    result += 7
+                    data[num] -= 1
+                    data[num+1] -= 1
+                    data[num+2] -= 1
                     break
-
-                result += 3
-                data[num] -= 1
-                break
-
+                elif data[num+1] > 0 and data[num] <= data[num+1]:
+                    result += 5
+                    data[num] -= 1
+                    data[num+1] -= 1
+                    break
+                else:
+                    result += 3
+                    data[num] -= 1
+                    break
+            elif (num+1) <= len(data)-1:
+                if data[num+1] > 0 and data[num] <= data[num+1]:
+                    result += 5
+                    data[num] -= 1
+                    data[num+1] -= 1
+                    break
+                else:
+                    result += 3
+                    data[num] -= 1
+                    break
             result += 3
             data[num] -= 1
             break
     c = sum(data)
+    if c == 0:
+        break
 
 print(result)
+a = time.time() - a
+print(a)
