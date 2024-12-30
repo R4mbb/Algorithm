d7b4e66152cbf903d65421b3a3f4e0a0a52d0f0a
R4mbb <yong010301@gmail.com>
2024-12-30 17:40:16 +0900

    2024-12-30

--- BOJ/Greedy/18185/ex.py
+++ BOJ/Greedy/18185/ex.py
@@ -1,21 +1,36 @@
-# 안풀림 ㅋㅋㅋ
-
+# 시간초과
 n = int(input())
 data = list(map(int, input().split()))
 
-data.sort(reverse=True)
-c = 0
 result = 0
+c = sum(data)
 
-for i in data:
-    if i == 0:
-        c = 0
-    elif i >= 1:
-        if c == 0:
-            result += 3*i
-            c = 1
-        else:
-            result += 2*i
+while c > 0:
+    for num in range(n):
+        if data[num] > 0:
+            if (num+1) <= len(data)-1:
+                if data[num+1] > 0 and data[num] <= data[num+1]:
+                    if (num+2) <= len(data)-1:
+                        if data[num+2] > 0 and data[num+1] <= data[num+2]:
+                            result += 7
+
+                            for j in range(num, num+3):
+                                data[j] -= 1
+                            break
+                
+                    result += 5
+
+                    for j in range(num, num+2):
+                        data[j] -= 1
+                    break
+
+                result += 3
+                data[num] -= 1
+                break
+
+            result += 3
+            data[num] -= 1
+            break
+    c = sum(data)
 
 print(result)
-        
